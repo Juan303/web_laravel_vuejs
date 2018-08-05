a8a2a9aae40942e7c4ccee4b88fa2f20107805b1
juan303 <gdf000@hotmail.com>
2018-08-05 21:44:49 +0200

    6-8_1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id')->unsigned();
             $table->string('name');
