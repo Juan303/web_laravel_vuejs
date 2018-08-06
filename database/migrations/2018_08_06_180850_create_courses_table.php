0fa2d4da571fa1292c2ea5530ca68eddc8a1f1b9
juan303 <gdf000@hotmail.com>
2018-08-06 20:26:49 +0200

    06-08-1

---
+++ database/migrations/2018_08_06_180850_create_courses_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Course;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+            $table->enum('status',[Course::PENDING, Course::PUBLISHED, Course::REJECTED])->default(Course::PENDING);
+            $table->boolean('previous_approved')->default(false);
+            $table->boolean('previous_rejected')->default(false);
+            
+            //friendly url
+            $table->string('slug');
+
+            //FK
+            $table->integer('teacher_id')->unsigned();
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->integer('level_id')->unsigned();
+            $table->foreign('level_id')->references('id')->on('levels');
+
+            $table->timestamps();
+
+            //borrado logico
+            $tables->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
