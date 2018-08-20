346f0c9eb5c087b72dc8aca8a87d3062d400e889
juan303 <gdf000@hotmail.com>
2018-08-20 02:33:47 +0200

    20-08-1

--- config/app.php
+++ config/app.php
@@ -208,7 +208,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'DataTables' => Yagra\DataTables\Facades\DataTables::class, 
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class
 
     ],
 
